22c7ed179973f66ff8f5d3dea191555a2dfa6abb
Ak@sh <107026623+am-akash1309@users.noreply.github.com>
2025-12-31 16:50:03 +0530

    Open Link in App when using phone

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,5 +9,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  openInstagram(event: Event) {
+    event.preventDefault();
+    const username = 'coimbatore_pet_adoption';
+    const appUrl = `instagram://user?username=${username}`;
+    const webUrl = `https://www.instagram.com/${username}/`;
 
+    // Simple detection for mobile devices
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      // Try to open in the app
+      window.location.href = appUrl;
+
+      // Fallback to web if the app doesn't open within a short time
+      setTimeout(() => {
+        window.open(webUrl, '_blank');
+      }, 500);
+    } else {
+      // Desktop - open web directly
+      window.open(webUrl, '_blank');
+    }
+  }
 }
