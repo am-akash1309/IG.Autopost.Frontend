5a418e41c62ac831c097562736440eacbac1a326
Ak@sh <107026623+am-akash1309@users.noreply.github.com>
2025-12-31 09:57:02 +0530

    UI Upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, ViewEncapsulation, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Router, NavigationEnd, RouterOutlet, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,14 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 })
 export class AppComponent {
   title = 'Coimbatore Pet Adoption';
+
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      if (isPlatformBrowser(this.platformId)) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
